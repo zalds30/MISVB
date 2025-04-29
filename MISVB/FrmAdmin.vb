609087de0107978e927ad404c37a34a20d0431bb
zalds30 <zalddfox26@gmail.com>
2025-04-30 07:35:39 +0800

    update

--- MISVB/FrmAdmin.vb
+++ MISVB/FrmAdmin.vb
@@ -1,18 +1,6 @@
 ﻿Public Class frmAdmin
     Private Sub btndashboard_Click(sender As Object, e As EventArgs) Handles btndashboard.Click
-        pagepanel.Controls.Clear()
-        Dim frm As New frmDashboard() ' <-- palitan ito ng tamang form
-        frm.TopLevel = False
-        frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
-        frm.Dock = DockStyle.Fill
-        pagepanel.Controls.Add(frm)
-        frm.Show()
 
-        panelform.Dock = DockStyle.Top
-        Dim myControl As New Dashboard() ' <-- siguraduhing ito ay tamang UserControl
-        myControl.Dock = DockStyle.Fill
-        panelform.Controls.Clear()
-        panelform.Controls.Add(myControl)
 
     End Sub
 
@@ -21,34 +9,10 @@
     End Sub
 
     Private Sub btnjobposting_Click(sender As Object, e As EventArgs) Handles btnjobposting.Click
-        pagepanel.Controls.Clear()
-        Dim frm As New frmJobpositons()
-        frm.TopLevel = False
-        frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
-        frm.Dock = DockStyle.Fill
-        pagepanel.Controls.Add(frm)
-        frm.Show()
 
-        panelform.Dock = DockStyle.Top
-        Dim myControl As New Jobpostions()
-        myControl.Dock = DockStyle.Fill
-        panelform.Controls.Clear()
-        panelform.Controls.Add(myControl)
     End Sub
 
     Private Sub btnapplication_Click(sender As Object, e As EventArgs) Handles btnapplication.Click
-        pagepanel.Controls.Clear()
-        Dim frm As New frmAdminDashboard()
-        frm.TopLevel = False
-        frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
-        frm.Dock = DockStyle.Fill
-        pagepanel.Controls.Add(frm)
-        frm.Show()
 
-        panelform.Dock = DockStyle.Top
-        Dim myControl As New AdminDashBoard()
-        myControl.Dock = DockStyle.Fill
-        panelform.Controls.Clear()
-        panelform.Controls.Add(myControl)
     End Sub
 End Class
