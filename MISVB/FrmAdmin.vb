3a86e3917179b7c3d24fc3815da21afaff498384
zalds30 <zalddfox26@gmail.com>
2025-04-30 07:32:50 +0800

    update

--- MISVB/FrmAdmin.vb
+++ MISVB/FrmAdmin.vb
@@ -1,16 +1,54 @@
 ﻿Public Class frmAdmin
     Private Sub btndashboard_Click(sender As Object, e As EventArgs) Handles btndashboard.Click
         pagepanel.Controls.Clear()
-        Dim frm As New frmDashboard()
+        Dim frm As New frmDashboard() ' <-- palitan ito ng tamang form
         frm.TopLevel = False
         frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
         frm.Dock = DockStyle.Fill
         pagepanel.Controls.Add(frm)
         frm.Show()
 
+        panelform.Dock = DockStyle.Top
+        Dim myControl As New Dashboard() ' <-- siguraduhing ito ay tamang UserControl
+        myControl.Dock = DockStyle.Fill
+        panelform.Controls.Clear()
+        panelform.Controls.Add(myControl)
+
     End Sub
 
     Private Sub frmAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.WindowState = Windows.Forms.FormWindowState.Maximized
+    End Sub
+
+    Private Sub btnjobposting_Click(sender As Object, e As EventArgs) Handles btnjobposting.Click
+        pagepanel.Controls.Clear()
+        Dim frm As New frmJobpositons()
+        frm.TopLevel = False
+        frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        frm.Dock = DockStyle.Fill
+        pagepanel.Controls.Add(frm)
+        frm.Show()
+
+        panelform.Dock = DockStyle.Top
+        Dim myControl As New Jobpostions()
+        myControl.Dock = DockStyle.Fill
+        panelform.Controls.Clear()
+        panelform.Controls.Add(myControl)
+    End Sub
+
+    Private Sub btnapplication_Click(sender As Object, e As EventArgs) Handles btnapplication.Click
+        pagepanel.Controls.Clear()
+        Dim frm As New frmAdminDashboard()
+        frm.TopLevel = False
+        frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        frm.Dock = DockStyle.Fill
+        pagepanel.Controls.Add(frm)
+        frm.Show()
+
+        panelform.Dock = DockStyle.Top
+        Dim myControl As New AdminDashBoard()
+        myControl.Dock = DockStyle.Fill
+        panelform.Controls.Clear()
+        panelform.Controls.Add(myControl)
     End Sub
 End Class
